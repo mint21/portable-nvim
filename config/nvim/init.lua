cb7ac281358dc6b017891a1c5f3ed2f7b97ff281
mint21 <66909360+mint21@users.noreply.github.com>
2026-01-11 19:09:12 +0100

    add lazy

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,6 +11,25 @@ vim.opt.termguicolors = true    -- True color support
 -- Leader key
 vim.g.mapleader = ","
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  -- Add plugins here
+})
+
 -- Basic keymaps
 vim.keymap.set("n", "<leader>w", ":w<CR>")      -- Save
 vim.keymap.set("n", "<leader>q", ":q<CR>")      -- Quit

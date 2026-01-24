6fd395462e6c8fbd571a28ff8df6a4765952b129
mint21 <66909360+mint21@users.noreply.github.com>
2026-01-24 14:58:42 +0100

    probe-rs for pico

--- config/nvim/lua/plugins/dap/probe-rs.lua
+++ config/nvim/lua/plugins/dap/probe-rs.lua
@@ -11,6 +11,8 @@ function M.setup(dap)
     }
 
     dap.configurations.rust = dap.configurations.rust or {}
+
+    -- ESP32-S3 config
     table.insert(dap.configurations.rust, {
         name = "ESP32-S3 (probe-rs)",
         type = "probe_rs",
@@ -20,9 +22,37 @@ function M.setup(dap)
             {
                 coreIndex = 0,
                 programBinary = function()
-                    return vim.fn.input("Path to ELF: ", vim.fn.getcwd() .. "/target/xtensa-esp32s3-none-elf/debug/",
-                        "file")
+                    return vim.fn.input("Path to ELF: ", vim.fn.getcwd() .. "/target/xtensa-esp32s3-none-elf/debug/", "file")
                 end,
+            }
+        },
+    })
+
+    -- RP2040 config
+    table.insert(dap.configurations.rust, {
+        name = "RP2040 Debug (probe-rs)",
+        type = "probe_rs",
+        request = "launch",
+        chip = "RP2040",
+        coreConfigs = {
+            {
+                coreIndex = 0,
+                programBinary = function()
+                    return vim.fn.input("Path to ELF: ", vim.fn.getcwd() .. "/target/thumbv6m-none-eabi/debug/", "file")
+                end,
+            }
+        },
+    })
+
+    table.insert(dap.configurations.rust, {
+        name = "RP2040 Release (probe-rs)",
+        type = "probe_rs",
+        request = "launch",
+        chip = "RP2040",
+        coreConfigs = {
+            {
+                coreIndex = 0,
+                programBinary = vim.fn.getcwd() .. "/target/thumbv6m-none-eabi/release/" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t"),
             }
         },
     })
